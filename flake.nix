7dbfe05f07210c8b53da4fb298b79fac3a18801d
Solace <nkumar301402@gmail.com>
2026-03-21 13:30:48 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,27 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations.nyxos = lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
-      };
-
-      homeConfigurations = {
-        frenny = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
-        };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "bak";
+            home-manager.users.frenny = import ./home.nix;
+          }
+        ];
       };
     };
 }
